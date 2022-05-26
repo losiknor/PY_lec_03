d32db9876259f88020e19ab58a1d159d09579d32
Larina <Losiknor@gmail.com>
2022-05-26 20:16:55 +0700

    Шестой шаг - это четвёртый с map

---
+++ lec_03_04_ переделанное с map.py
@@ -0,0 +1,9 @@
+def where(f, col):
+    return [x for x in col if f(x)]
+
+data = '1 2 3 5 8 15 23 38'.split()
+
+res = map(int, data)
+res = where(lambda x: not x % 2, res)
+res = list(map(lambda x: (x, x**2), res))
+print(res)
